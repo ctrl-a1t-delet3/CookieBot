f3e990dddc7c88233397e464a55042e433792e3d
AJ Williams <77739597+ctrl-a1t-delet3@users.noreply.github.com>
2021-07-11 01:16:22 -0500

    Add files via upload
    First commit. Nothing but basic !ping command.

---
+++ BotFolders/CookieBot/CookieBot/Modules/Commands.cs
@@ -0,0 +1,40 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+using Discord;
+
+namespace CookieBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        public Commands()
+        {
+            var File = new File();
+            var FileWriter = new FileWriter();
+        }
+
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Pong");
+        }
+
+        [Command("changeprefix")]
+        [RequireUserPermission(GuildPermission.BanMembers, ErrorMessage = "Insufficient permissions")]
+        public async Task ChangePrefix(string newPrefix = null)
+        {
+            if (newPrefix == null)
+            {
+                await ReplyAsync("Please enter a prefix to change to!");
+            }
+            if (newPrefix.Length > 1)
+            {
+                await ReplyAsync("Please enter a singular character to change the prefix to! (ex. !, ?, ., etc.");
+            }
+            else
+            {
+                Startup.prefix = newPrefix;
+                await ReplyAsync("Change sucessful!");
+            }
+        }
+    }
+}
